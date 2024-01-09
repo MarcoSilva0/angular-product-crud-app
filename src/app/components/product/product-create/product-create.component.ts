e861f3a96e98c03f8bacfdaa5722ba6ebdb0c608
MarcoSilva0 <marcoantonio02016@outlook.com>
2024-01-09 15:07:32 -0300

    feat: add product service

--- src/app/components/product/product-create/product-create.component.ts
+++ src/app/components/product/product-create/product-create.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { ProductService } from "../product.service";
+import { MatButtonModule } from "@angular/material/button";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-product-create',
+  selector: "app-product-create",
   standalone: true,
-  imports: [],
-  templateUrl: './product-create.component.html',
-  styleUrl: './product-create.component.css'
+  imports: [MatButtonModule],
+  templateUrl: "./product-create.component.html",
+  styleUrl: "./product-create.component.css",
 })
 export class ProductCreateComponent {
+  constructor(private productService: ProductService, private router: Router) {}
 
+  ngOnInit(): void {}
+
+  createProduct(): void {
+    this.productService.showMessage("Operação executada com sucesso");
+  }
+  cancel(): void {
+    this.router.navigate(["/products"]);
+  }
 }
