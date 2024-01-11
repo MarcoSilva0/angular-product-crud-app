e1ba62632c3c7b1a4616a55c346665f7df582982
MarcoSilva0 <marcoantonio02016@outlook.com>
2024-01-11 14:23:58 -0300

    feat: Product list page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from "@angular/core";
+import { Component, LOCALE_ID } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HeaderComponent } from "./components/template/header/header.component";
 
@@ -6,6 +6,11 @@ import { MatToolbarModule } from "@angular/material/toolbar";
 import { FooterComponent } from "./components/template/footer/footer.component";
 import { NavComponent } from "./components/template/nav/nav.component";
 import { HttpClientModule } from "@angular/common/http";
+
+import localePt from "@angular/common/locales/pt";
+import { registerLocaleData } from "@angular/common";
+
+registerLocaleData(localePt);
 
 @Component({
   selector: "app-root",
@@ -17,6 +22,12 @@ import { HttpClientModule } from "@angular/common/http";
     FooterComponent,
     NavComponent,
     HttpClientModule,
+  ],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: "pt-BR",
+    },
   ],
   templateUrl: "app.component.html",
 })
--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -1,20 +1,22 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { Product } from "../product.model";
 import { ProductService } from "../product.service";
 import { Router } from "@angular/router";
 import { MatButtonModule } from "@angular/material/button";
-import { NgForOf } from "@angular/common";
+import { CurrencyPipe, NgForOf } from "@angular/common";
+import { MatTableModule } from "@angular/material/table";
 
 @Component({
   selector: "app-product-read",
   standalone: true,
-  imports: [MatButtonModule, NgForOf],
+  imports: [MatButtonModule, NgForOf, CurrencyPipe, MatTableModule],
   providers: [ProductService],
   templateUrl: "./product-read.component.html",
   styleUrl: "./product-read.component.css",
 })
 export class ProductReadComponent {
   products: Product[] = [];
+  displayedColumns = ["id", "name", "price"];
 
   constructor(private router: Router, private productService: ProductService) {}
 
